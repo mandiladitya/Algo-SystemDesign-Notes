98e742858259fce35599caa7d7a29aedb33249dd
Aditya Mandil <46640551+mandiladitya@users.noreply.github.com>
2020-10-03 00:04:39 +0530

    Update Reverse_Array_INPLACE.cpp

--- Tricks/Reverse_Array_INPLACE.cpp
+++ Tricks/Reverse_Array_INPLACE.cpp
@@ -1,3 +1,9 @@
+/*
+An in-place algorithm is an algorithm that does not need an extra space and produces an output 
+in the same memory that contains the data by transforming the input ‘in-place’. 
+However, a small constant extra space used for variables is allowed. 
+*/
+
 void revereseArray(int arr[], int n) 
 { 
    for (int i=0; i<n/2; i++) 
