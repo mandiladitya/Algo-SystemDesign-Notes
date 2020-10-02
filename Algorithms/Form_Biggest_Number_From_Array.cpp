4a4bffbe66cfd1b489f1906b5492d13e2ff46f18
Aditya Mandil <46640551+mandiladitya@users.noreply.github.com>
2020-10-02 21:51:44 +0530

    Create Form_Biggest_Number_From_Array.cpp

---
+++ Algorithms/Form_Biggest_Number_From_Array.cpp
@@ -0,0 +1,19 @@
+// Own Comparator
+int myCompare(string X, string Y) 
+{ 
+    // first append Y at the end of X 
+    string XY = X.append(Y); 
+  
+    // then append X at the end of Y 
+    string YX = Y.append(X); 
+  
+    // Now see which of the two formed numbers is greater 
+    return XY.compare(YX) > 0 ? 1: 0; 
+} 
+void printLargest(vector<string> arr) 
+{ 
+    sort(arr.begin(), arr.end(), myCompare); 
+  
+    for (int i=0; i < arr.size() ; i++ ) 
+        cout << arr[i]; 
+} 
